7a61ad1830e7d0d11e98bab184284a59bda81073
SamuelV1 <s4muelvic@outlook.com>
2022-11-18 20:47:41 -0300

    dummy test component

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}> 
+      <ImageBackground source={require('./assets/images/ModelX.jpeg')} style={styles.image}/>
+          {/*header */}
+          <View style={styles.tittles}>
+            <Text style={styles.tittle}>Model S</Text>
+            <Text style={styles.subtitle}>Starting at $69,420</Text>
+
+          </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  tittles:{
+    marginTop: '30%',
+    width: "100%",
+    alignItems: 'center',
+  },
+  subtitle: {
+    color: "#5c5e62",
+    fontSize: "16"
+  },
+  tittle: {
+    fontSize: "40",
+    fontWeight: "600",
+  },
+  image:{
+    width: "100%",
+    height: "100%",
+    resizeMode: 'cover',
+    position: "absolute"
+  }
 });
